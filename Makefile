6597568ddb557ccbb13cbbe1fecb0c1e5f4438b7
Haruki Gonai <hg2541@columbia.edu>
2023-01-11 14:59:42 -0500

    Generate cscope Database of Minimum Files Required to Compile

--- Makefile
+++ Makefile
@@ -2,6 +2,6 @@
 subsystem:
 	cd linux-stable && \
 	$(MAKE) mrproper > /dev/zero && \
-	$(MAKE) ARCH=${ARCHNAME} CROSS_COMPILE=aarch64-linux-gnu- tinyconfig > /dev/zero && \
+	$(MAKE) ARCH=${ARCHNAME} CROSS_COMPILE=aarch64-linux-gnu- defconfig > /dev/zero && \
 	$(MAKE) clean > /dev/zero && \
 	$(MAKE) ARCH=${ARCHNAME} CROSS_COMPILE=aarch64-linux-gnu- -n -k
--- main.py
+++ main.py
@@ -3,24 +3,76 @@ import re
 import subprocess
 import sys
 
-def filter_files(li):
-    files = set()
-    for filename in li:
-        if ".o" in filename or ".c" in filename:
-            res = os.path.splitext(filename)[0]
-            files.add(res)
-    return files
-
-def main():
-    bashCommand = f'make ARCHNAME={sys.argv[1]}'
-    p = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+def run_bash(cmd):
+    p = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     s = p.communicate()
     res = p.wait()
+    return s
+
+def sys_dirs(arch):
+    return [
+        f'arch/{arch}/include',
+        f'arch/{arch}/include/generated',
+        "include",
+        f'arch/{arch}/include/uapi',
+        f'arch/{arch}/include/generated/uapi',
+        "include/uapi",
+        "include/generated/uapi",
+    ]
+
+def main():
+    arch = sys.argv[1]
+    cmd = f'make ARCHNAME={arch}'
+    s = run_bash(cmd)
 
     stdout_res = str(s[0])
     li = [x.group() for x in re.finditer(r'gcc (.*?) -c -o \S+ \S+', stdout_res)]
-    li = sorted(list(set([x.split()[-1].strip(";'\\") for x in li])))
-    print("\n".join(li))
+    li = set(["linux-stable/" + x.split()[-1].strip(";'\\") for x in li])
+
+    sys_dirs_li = sys_dirs(arch)
+
+    files_to_process = li
+    files_processed = set()
+    while len(files_to_process) != 0:
+        nxt_files_to_process = set()
+        for path in files_to_process:
+            if os.path.exists(path):
+                f = open(path)
+                for line in f:
+                    if line.startswith("#include") and ".h" in line:
+                        tokens = line.split()
+                        filename = tokens[1].strip().strip('"')
+
+                        if "<" in filename and ">" in filename:
+                            stripped_file = filename.strip().strip("<>")
+                            file_found = False
+                            for sys_dir in sys_dirs_li:
+                                candidate = f'linux-stable/{sys_dir}/{stripped_file}'
+                                if os.path.exists(candidate):
+                                    nxt_files_to_process.add(candidate)
+                                    file_found = True
+                                    break
+
+                            if not file_found:
+                                # print("Couldn't find", file)
+                                pass
+                        else:
+                            dirname = os.path.dirname(path)
+                            unsimplified_path = dirname + "/" + filename
+                            simplified_path = os.path.normpath(unsimplified_path)
+                            nxt_files_to_process.add(simplified_path)
+
+        files_processed = files_processed.union(files_to_process)
+        files_to_process = nxt_files_to_process - files_processed
+
+    f = open("linux-stable/cscope.files", "w")
+    for file in files_processed:
+        f.write(file[len("linux-stable")+1:] + "\n")
+
+    os.chdir('linux-stable')
+
+    cmd = f'cscope -b -q -k'
+    s = run_bash(cmd)
 
 main()
 
